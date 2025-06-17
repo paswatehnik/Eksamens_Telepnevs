ee5890781886fa99141748ce79b7f8ca6860a626
paswatehnik <mihailpetrovich789@gmail.com>
2025-06-18 02:47:19 +0300

    pievinoju 1.praktisko uzdevumu maciba materiala, nodzesu tuksus failus un izveidoju atseviskus projektus katram uzdevumam

---
+++ praktiskaisUzdevums1/praktiskaisUzdevums1/praktiskaisUzdevums1.py
@@ -0,0 +1,10 @@
+﻿# Inicializē skaitītāju
+i = 2
+
+# While cikls, kas strādā, kamēr i <= 20
+while i <= 20:
+    if i % 2 != 0:  # Pārbauda, vai skaitlis ir nepāra
+        i += 1      # Palielina skaitītāju
+        continue     # Izlaiž iterāciju, ja skaitlis ir nepāra
+    print(i)        # Izvada pāra skaitli
+    i += 1          # Palielina skaitītāju
--- pythonTest/pythonTest/pythonTest.py
+++ pythonTest/pythonTest/pythonTest.py
@@ -1,6 +1,1 @@
-﻿i = 1
-while i <= 5:
-    print(i)
-    i += 1
-else:
-    print("Cikls veiksmīgi pabeigts!")  # Izvadīsies, ja nebūs 'break'
+﻿
