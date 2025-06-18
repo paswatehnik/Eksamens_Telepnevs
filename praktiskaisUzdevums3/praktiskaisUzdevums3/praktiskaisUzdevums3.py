0c8e646bbf6bb78f575ba8f69a8ae85dc9812499
paswatehnik <mihailpetrovich789@gmail.com>
2025-06-18 03:59:55 +0300

    pievinoju 3.praktisko uzdevumu maciba materiala (pabeidzu veidosanu maciba materiala)

--- praktiskaisUzdevums3/praktiskaisUzdevums3/praktiskaisUzdevums3.py
+++ praktiskaisUzdevums3/praktiskaisUzdevums3/praktiskaisUzdevums3.py
@@ -1,1 +1,16 @@
-﻿
+﻿# Lietotājs ievada skaitli
+skaitlis = int(input("Ievadiet veselu skaitli: "))
+
+# Inicializē mainīgos
+summa = 0
+i = 1
+
+# Pārbauda, vai skaitlis ir negatīvs
+if skaitlis < 0:
+    print("Nepareiza ievade!")
+else:
+    # While cikls, kas summē skaitļus no 1 līdz ievadītajam skaitlim
+    while i <= skaitlis:
+        summa += i  # Pieskaita i vērtību summai
+        i += 1      # Palielina skaitītāju
+    print(f"Summa no 1 līdz {skaitlis} ir: {summa}")
