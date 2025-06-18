626fb222379bb81b098baf3b28ccdfc53f77e5e3
paswatehnik <mihailpetrovich789@gmail.com>
2025-06-18 03:14:34 +0300

    pievinoju 2.praktisko uzdevumu maciba materiala

--- praktiskaisUzdevums2/praktiskaisUzdevums2/praktiskaisUzdevums2.py
+++ praktiskaisUzdevums2/praktiskaisUzdevums2/praktiskaisUzdevums2.py
@@ -1,1 +1,13 @@
+﻿# Lietotājs ievada skaitli
+skaitlis = int(input("Ievadiet veselu skaitli: "))
 
+# Inicializē skaitītāju
+i = 1
+
+# While cikls, kas izvada skaitļus no 1 līdz ievadītajam skaitlim
+while i <= skaitlis:
+    print(i)
+    i += 1  # Palielina skaitītāju par 1
+else:
+    # Izvada paziņojumu, ja cikls nav pārtraukts ar 'break'
+    print("Skaitīšana pabeigta!")
